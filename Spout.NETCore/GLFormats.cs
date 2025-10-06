48e42cf7d8d58044fc9e703cec38bac8a89ba619
AWAS666 <AWAS6@protonmail.com>
2025-10-06 09:38:12 +0200

    feat: move common glformats to this repo

---
+++ Spout.NETCore/GLFormats.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spout.NETCore;
+
+
+/// <summary>
+/// formats to be used with OpenGL -> there are many more but these are the most common
+/// </summary>
+public static class GLFormats
+{
+    public const uint RED = 6403;
+    public const uint GREEN = 6404;
+    public const uint BLUE = 6405;
+    public const uint ALPHA = 6406;
+    public const uint RGB = 6407;
+    public const uint RGBA = 6408;
+}
